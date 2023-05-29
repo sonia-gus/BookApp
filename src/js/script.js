df9078c72e67d8293d79c46c78b593921491b184
Sonia Guszpit <guszpits@gmail.com>
2023-05-29 14:16:32 +0200

    add function initActions

--- src/js/script.js
+++ src/js/script.js
@@ -1,21 +1,39 @@
 {
-    const templates = {
-      bookTemplate: Handlebars.compile(document.querySelector('#template-book').innerHTML),
-    }
-    const booksList = document.querySelector('.books-list');
-    console.log(templates.bookTemplate);
-    const booksData = dataSource.books;
+  const templates = {
+    bookTemplate: Handlebars.compile(document.querySelector('#template-book').innerHTML),
+  };
+  const booksList = document.querySelector('.books-list');
+  const booksData = dataSource.books;
+  let favouriteBooks = [];
   
-    function render(){
-      //const thisBook = this;
-      //thisBook.data = dataSource;
-  
-       for(let book in booksData){
-          console.log(booksData[book]);
-          const generatedHTML = templates.bookTemplate(booksData[book]);
-          const createdBook = utils.createDOMFromHTML(generatedHTML);
-          booksList.appendChild(createdBook);
-       }   
-    }
-    render();
+  function render(){
+
+    for(let book in booksData){
+      const generatedHTML = templates.bookTemplate(booksData[book]);
+      const createdBook = utils.createDOMFromHTML(generatedHTML);
+      booksList.appendChild(createdBook);
+    }   
   }
+
+  function initActions(){
+    const bookImages = document.querySelectorAll(".book__image")
+    for(let bookImage in bookImages){
+      const singularImage = bookImages[bookImage];
+      favouriteBooks.push(singularImage);
+    }
+    favouriteBooks = favouriteBooks.slice(0, -6);
+    console.log(favouriteBooks);
+    for(let index in favouriteBooks){
+      let favouriteBooksElement = favouriteBooks[index];
+      favouriteBooksElement.addEventListener('dblclick', function(event){
+        event.preventDefault()
+        favouriteBooksElement.classList.add("favorite");
+      });
+    }
+    
+  }
+
+
+  render();
+  initActions();
+}
